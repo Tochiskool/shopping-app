d63eb19159b854c2cc52043e0d1d907c48dd87f4
Tochiskool <mrchristian79@yahoo.com>
2020-10-22 16:06:47 +0200

    Filtered component partially done and technical changes on data.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //Feature-1
 import React from 'react';
+import Filter from './components/Filter';
 import Products from './components/Products';
 import data from "./data.json";
 
@@ -8,11 +9,52 @@ class App extends React.Component{
     super(props)
     this.state = {
       products: data.products,
-      size: "",
-      sort:""
+      size:"",
+      sort: ""
     }
- }
+  }
+  sortProducts=(event)=>{
+    //implement 
+    const sort = event.target.value;
+    console.log(event.target.value)
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>        
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            :
+            sort === "highest"
+              ? a.price < b.price
+                ? 1
+                : -1
+              : a._id > b._id
+                ? 1
+                :1
+        )
+    })) 
+  }
+  filterProducts =(event)=>{
+    //implement size
+    console.log(event.target.value)
+    if (event.target.value === "") {
+      this.setState({
+        size: event.target.value,
+        products: data.products
+      });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products:data.products.filter(product => product.availableSizes.indexOf(event.target.value) >= 0)
+     })
+    }
+  }
+  
   render() {
+    
      return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +63,14 @@ class App extends React.Component{
       <main>
            <div className="content">
              <div className="main">
+               <Filter
+                 count={this.state.products.length}
+                 size={this.state.size}
+                 sort={this.state.sort}
+                 filterProducts={this.filterProducts}
+                 sortProducts={this.sortProducts}
+               />
+   
                <Products products={this.state.products}/>
              </div>
              <div className="sidebar">
